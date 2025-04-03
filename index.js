cc084db13e0fb17b0ab8700fd34da33e6a99b57d
l-efa <lefa.sahi@gmail.com>
2025-04-03 14:55:04 +0300

    projekti valmis ja linttaus tehty

--- index.js
+++ index.js
@@ -1,167 +1,139 @@
-import express, { response } from "express";
-import logger from "morgan";
-import PhoneBook from "./mongo.js";
+import express from 'express'
+import logger from 'morgan'
+import PhoneBook from './mongo.js'
 
-const app = express();
-app.use(express.static("dist"));
+const app = express()
+//app.use(express.static("dist"));
 
-app.use(express.json());
-logger.token("body", (req) => {
-  return JSON.stringify(req.body);
-});
+app.use(express.json())
+logger.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
-app.use(logger(":method :url :status :body"));
+app.use(logger(':method :url :status :body'))
 
-/*
-let phoneNumbers = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
+const errorHandler = (error, response, next) => {
+  console.error(error.message)
 
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-*/
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.get("/api/phonenumbers", (request, response) => {
+app.get('/api/phonenumbers', (response) => {
   PhoneBook.find({}).then((results) => {
-    console.log("results!!!: ", results);
-    response.send(results);
-  });
-});
+    console.log('results!!!: ', results)
+    response.send(results)
+  })
+})
 
-app.get("/api/phonenumbers/:id", (request, response, next) => {
-  const id = request.params.id;
+app.get('/api/phonenumbers/:id', (request, response, next) => {
+  const id = request.params.id
   PhoneBook.findById(id)
     .then((results) => {
-      console.log(results);
+      console.log(results)
       if (results) {
-        response.json(results);
+        response.json(results)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/info", (request, response) => {
-  let numbers = 0;
+app.get('/api/info', (request, response) => {
+  let numbers = 0
   PhoneBook.find({})
     .then((results) => {
-      numbers = results.length;
-      console.log("numbers: ", numbers);
-      const d = new Date();
+      numbers = results.length
+      console.log('numbers: ', numbers)
+      const d = new Date()
       response.send(
         `<p>Phonebook has info for ${numbers} people </p> <p>${d}</p>`
-      );
+      )
     })
     .catch((error) => {
-      console.log(error, error.name);
-    });
-});
+      console.log(error, error.name)
+    })
+})
 
-app.post("/api/phonenumbers", (request, response, next) => {
-  const body = request.body;
-  console.log("body: ", body);
-  let log;
+app.post('/api/phonenumbers', (request, response, next) => {
+  const body = request.body
+  console.log('body: ', body)
+  let log
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name or number missing",
-    });
+      error: 'name or number missing',
+    })
   }
 
   if (!body.id) {
-    const id = String(Math.floor(Math.random() * 1000));
+    const id = String(Math.floor(Math.random() * 1000))
 
     log = new PhoneBook({
       id: id,
       name: body.name,
       number: body.number,
-    });
+    })
   } else {
     log = new PhoneBook({
       id: body.id,
       name: body.name,
       number: body.number,
-    });
+    })
   }
 
-  console.log("log: ", log);
+  console.log('log: ', log)
 
   log
     .save()
     .then((result) => {
-      console.log("log saved to database", log);
-      response.send(result);
+      console.log('log saved to database', log)
+      response.send(result)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/phonenumbers/:id", (request, response, next) => {
-  const id = request.params.id;
-  console.log(id);
+app.delete('/api/phonenumbers/:id', (request, response, next) => {
+  const id = request.params.id
+  console.log(id)
 
   PhoneBook.findByIdAndDelete(id)
     .then((result) => {
-      console.log(result);
-      response.status(204).end();
+      console.log(result)
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/phonenumbers/:id", (request, response, next) => {
-  const { name, number } = request.body;
-  console.log("serverissä id: ", request.body._id);
-  console.log("params id: ", request.params.id);
+app.put('/api/phonenumbers/:id', (request, response, next) => {
+  const { name, number } = request.body
+  console.log('serverissä id: ', request.body._id)
+  console.log('params id: ', request.params.id)
   PhoneBook.findById(request.params.id)
     .then((bookListing) => {
       if (!bookListing) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      bookListing.name = name;
-      bookListing.number = number;
+      bookListing.name = name
+      bookListing.number = number
 
       return bookListing.save().then((updatedBookListing) => {
-        response.json(updatedBookListing);
-      });
+        response.json(updatedBookListing)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,19 +1,17 @@
-import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose from 'mongoose'
 
-const url = process.env.MONGODB_URL;
-mongoose.set("strictQuery", false);
-console.log("connecting to mongodb..");
+const url = process.env.MONGODB_URL
+mongoose.set('strictQuery', false)
+console.log('connecting to mongodb..')
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to mongodb");
-  })
+  .then(console.log('connected to mongodb'))
   .catch((error) => {
-    console.log("Error connecting to mongodb", error.message);
-  });
+    console.log('Error connecting to mongodb', error.message)
+  })
 
 const phoneNumberSchema = new mongoose.Schema({
   id: Number,
@@ -22,10 +20,21 @@ const phoneNumberSchema = new mongoose.Schema({
     minlength: 3,
     requierd: true,
   },
-  number: Number,
-});
+  number: {
+    type: Number,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^(?:\d{2}-\d{7,9}|\d{3}-\d{7,8})$/.test(v)
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Format should be: 123-1234567, 123-12345678, 12-12345678, 12-123456789`,
+    },
+    required: [true, 'User phone number required'],
+  },
+})
 
-export default mongoose.model("PhoneNumber", phoneNumberSchema);
+export default mongoose.model('PhoneNumber', phoneNumberSchema)
 
 /*
 if (process.argv.length <= 3) {
